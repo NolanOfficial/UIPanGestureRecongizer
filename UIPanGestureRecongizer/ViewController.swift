6789d0909a9bfea2e034cc8dd43ac6361d40d192
Nolan Fuchs <nolanfuchs@nolans-mbp.domain>
2019-01-06 23:21:52 -0700

    - Drag a file over to trash to delete it. Shows the use of Pan Gesture Recognizers.

--- UIPanGestureRecongizer/ViewController.swift
+++ UIPanGestureRecongizer/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var file: UIImageView!
+    @IBOutlet weak var trashCan: UIImageView!
+    
+    var fileViewOrigin: CGPoint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addPanGesture(view: file)
+        fileViewOrigin = file.frame.origin
     }
 
+    
+    func addPanGesture(view: UIView) {
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
+        
+    }
+    
+    @objc func handlePan(sender: UIPanGestureRecognizer) {
+        let fileView = sender.view
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            
+            fileView?.center = CGPoint(x: (fileView?.center.x)! + translation.x, y: (fileView?.center.y)! + translation.y)
+             sender.setTranslation(CGPoint.zero, in: view)
+            
+        case .ended:
+            
+            if fileView!.frame.intersects(trashCan.frame) {
+                
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.file.alpha = 0.0
+                    })
+                self.file.removeFromSuperview()
+                
+            } else {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.file.frame.origin = self.fileViewOrigin
+                    })
+            }
+            
+        default:
+            break
+        }
+        
+        
+    }
 
 }
 
